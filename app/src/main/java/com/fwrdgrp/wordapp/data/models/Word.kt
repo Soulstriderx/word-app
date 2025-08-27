5e5dfe8d50f6e411da1997c074efcafec092a367
Ephemera <kwanghaualex24s@forward.edu.my>
2025-08-27 15:27:42 +0800

    Word Model - added Date to the model

--- app/src/main/java/com/fwrdgrp/wordapp/data/models/Word.kt
+++ app/src/main/java/com/fwrdgrp/wordapp/data/models/Word.kt
@@ -1,12 +1,15 @@
 package com.fwrdgrp.wordapp.data.models
 
+import java.util.Date
+
 data class Word(
     val title: String,
     val meaning: String,
     val synonym: String? = null,
     val details: String? = null,
     val status: Status = Status.INCOMPLETE,
-    val id: Int? = null
+    val id: Int? = null,
+    val date: Date = Date()
 )
 enum class Status{
     COMPLETE, INCOMPLETE
