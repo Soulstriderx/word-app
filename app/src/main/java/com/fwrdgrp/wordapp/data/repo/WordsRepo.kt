1a844788b7f34c02fc3ea95ff058e0e0ec5125a2
Dominic <famdominic1218@gmail.com>
2025-08-27 15:35:39 +0800

    created WordsRepo

---
+++ app/src/main/java/com/fwrdgrp/wordapp/data/repo/WordsRepo.kt
@@ -0,0 +1,44 @@
+package com.fwrdgrp.wordapp.data.repo
+
+import com.fwrdgrp.wordapp.data.models.Word
+
+class WordsRepo private constructor() {
+    val map = mutableMapOf<Int, Word>()
+    var counter = 0
+
+    fun add(word: Word) {
+        counter++
+        map[++counter] = word.copy(id = counter)
+    }
+
+    fun getWordById(id: Int): Word? {
+        return map[id]
+    }
+
+    fun getWords() = map.values.toList()
+
+    fun deleteWord(id: Int) {
+        map.remove(id)
+    }
+
+    fun updateWord(id: Int, word: Word) {
+        map[id] = word
+    }
+
+    fun createRandomWords(n: Int) {
+        repeat(n) {
+            map[++counter] = Word(title = "Title $it", meaning = "Meaning $it")
+        }
+    }
+
+    companion object {
+        private var instance: WordsRepo? = null
+
+        fun getInstance(): WordsRepo {
+            if (instance == null) {
+                instance = WordsRepo()
+            }
+            return instance!!
+        }
+    }
+}
