1d3a470199dfb5dbeeb114971d90078401d58ad4
KXLeong <peterleong843@gmail.com>
2025-08-27 16:22:03 +0800

    created HomeFragment/HomeFragmentViewModel/fragment_home.xml (left setupAdapter in HomeFragment)

--- app/src/main/java/com/fwrdgrp/wordapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/fwrdgrp/wordapp/ui/home/HomeViewModel.kt
@@ -1,7 +1,32 @@
 package com.fwrdgrp.wordapp.ui.home
 
 import androidx.lifecycle.ViewModel
+import com.fwrdgrp.wordapp.data.models.Word
+import com.fwrdgrp.wordapp.data.repo.WordsRepo
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 
-class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class HomeViewModel(
+    private val repo: WordsRepo = WordsRepo.getInstance()
+) : ViewModel() {
+    private val _words = MutableStateFlow<List<Word>>(emptyList())
+    val words: StateFlow<List<Word>> = _words
+
+    init {
+        getWords()
+    }
+
+    fun getWords() {
+        _words.update { repo.getWords() }
+    }
+
+    fun refresh() {
+        getWords()
+    }
+
+    fun deleteWord(word: Word) {
+        repo.deleteWord(word.id!!)
+        refresh()
+    }
 }
