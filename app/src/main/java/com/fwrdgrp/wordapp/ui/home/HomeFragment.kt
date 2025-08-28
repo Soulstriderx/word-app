a794b27df0d5687fdad995fac45f5c905e142d5f
Ephemera <kwanghaualex24s@forward.edu.my>
2025-08-28 14:54:58 +0800

    Added bg color to bottom nav, made CompletedWordFragment, CompletedWordViewModel, BaseHomeFragment, BaseHomeViewModel, added icon to bottom nav menu, edited fragmentHomeXML, Tab Navigation logic in MainActivity, nav-graph updated. Added some icons and word filters for Home and CompletedWords viewModels

--- app/src/main/java/com/fwrdgrp/wordapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/fwrdgrp/wordapp/ui/home/HomeFragment.kt
@@ -1,61 +1,7 @@
 package com.fwrdgrp.wordapp.ui.home
 
 import androidx.fragment.app.viewModels
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.setFragmentResultListener
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.fwrdgrp.wordapp.adapter.WordsAdapter
-import com.fwrdgrp.wordapp.databinding.FragmentHomeBinding
-import kotlinx.coroutines.launch
 
-class HomeFragment : Fragment() {
-    private lateinit var binding: FragmentHomeBinding
-    private val viewModel: HomeViewModel by viewModels()
-    private lateinit var adapter: WordsAdapter
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding =  FragmentHomeBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        setupAdapter()
-
-        lifecycleScope.launch {
-            viewModel.words.collect {
-                adapter.setWords(it)
-                binding.llEmpty.visibility = if(it.isEmpty()) View.VISIBLE else View.GONE
-            }
-        }
-        binding.fabAdd.setOnClickListener{
-
-        }
-        setFragmentResultListener("manage_item"){ _, result ->
-            val shouldRefresh = result.getBoolean("refresh")
-            if(shouldRefresh)viewModel.refresh()
-        }
-    }
-
-    fun setupAdapter() {
-        adapter = WordsAdapter(
-            emptyList(),
-            onPress = { val action = HomeFragmentDirections.actionHomeToWordDetail(it.id!!)
-                findNavController().navigate(action) }
-        )
-
-        binding.rvWords.adapter = adapter
-        binding.rvWords.layoutManager = LinearLayoutManager(this.context)
-    }
-
-
-
+class HomeFragment : BaseHomeManageFragment() {
+    override val viewModel: HomeViewModel by viewModels()
 }
